ea3cae4754bd0486585b5a6ff8ac01445e09f1c0
DomieBett <dbett49@gmail.com>
2017-07-23 23:01:38 +0300

    [Feature] Add 'create_db' command line command

--- manage.py
+++ manage.py
@@ -11,6 +11,10 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def create_db():
+    """Creates the db tables."""
+    db.create_all()
 
 if __name__ == '__main__':
     manager.run()
