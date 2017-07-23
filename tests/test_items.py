944229bfad638136d1f7a551f87d96792b58d798
DomieBett <dbett49@gmail.com>
2017-07-23 23:02:10 +0300

    [Feature] Add tests for add and delete bucketlist items

---
+++ tests/test_items.py
@@ -0,0 +1,29 @@
+from . test import BaseTestCase
+from bucketlist.models import ListItem
+import json
+
+
+class TestCase(BaseTestCase):
+
+    def test_item_is_added(self):
+        self.add_bucketlist("Bucketlist1")
+        response = self.add_bucketlist_item(1, "Run a marathon.")
+        data = json.loads(response.data.decode())
+        list_items = ListItem.query.all()
+        self.assertEqual(len(list_items), 1)
+        self.assertEqual(data['status'], 'success')
+        self.assertEqual(data['bucketlist_id'], 1)
+        self.assertEqual(data['name'], "Run a marathon.")
+        self.assertEqual(data['message'], "Item added successfully")
+
+    def test_item_is_deleted(self):
+        self.add_bucketlist("Bucketlist1")
+        self.add_bucketlist_item(1, "Run a marathon")
+        self.add_bucketlist_item(1, "Ride a horse")
+        response = self.delete_bucketlist_item(id=1, item_id=1)
+        data = json.loads(response.data.decode())
+        list_items = ListItem.query.all()
+        self.assertEqual(len(list_items), 1)
+        self.assertEqual(data['status'], 'success')
+        self.assertEqual(data['bucketlist_id'], 1)
+        self.assertEqual(data['message'], "Successfully deleted." )
