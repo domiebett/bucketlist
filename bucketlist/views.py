6079cfdb2c5eb245ae27daec6c9e768bf585d5ed
DomieBett <dbett49@gmail.com>
2017-07-23 08:45:20 +0300

    [Feature] Add feature to login user

--- bucketlist/views.py
+++ bucketlist/views.py
@@ -56,3 +56,38 @@ def create_app(config_name):
                     'message': 'User already exists. Please Log in.',
                 }
                 return jsonify(response)
+
+    @api.route('/auth/login')
+    class Login(Resource):
+        """
+        User Login Resource
+        """
+        def post(self):
+            # get the post data
+            post_data = request.get_json()
+            try:
+                email = post_data.get('email')
+                password = post_data.get('password')
+                user = User.query.filter_by(
+                    email=email).first()
+                if user and user.is_correct_password(password):
+                    auth_token = user.encode_auth_token(user.id)
+                    if auth_token:
+                        responseObject = {
+                            'status': 'success',
+                            'message': 'Successfully logged in.',
+                            'auth_token': auth_token.decode()
+                        }
+                        return jsonify(responseObject)
+                else:
+                    return jsonify({
+                        'status': 'fail',
+                        "message": "Access Denied. Login Again"
+                    })
+            except Exception as e:
+                print(e)
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Try again'
+                }
+                return jsonify(responseObject)
