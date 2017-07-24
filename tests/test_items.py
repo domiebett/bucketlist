66e63b15d4e025f3797e46dec86940b073a0f89e
DomieBett <dbett49@gmail.com>
2017-07-24 12:49:56 +0300

    [Feature] Add test updating bucketlist items

--- tests/test_items.py
+++ tests/test_items.py
@@ -25,5 +25,14 @@ class TestCase(BaseTestCase):
         list_items = ListItem.query.all()
         self.assertEqual(len(list_items), 1)
         self.assertEqual(data['status'], 'success')
-        self.assertEqual(data['bucketlist_id'], 1)
         self.assertEqual(data['message'], "Successfully deleted." )
+
+    def test_item_is_updated(self):
+        self.add_bucketlist("Bucketlist1")
+        self.add_bucketlist_item(1, "Run a marathon")
+        response = self.update_bucketlist_item(id=1, item_id=1, name="Sky dive")
+        data = json.loads(response.data.decode())
+        db_item = ListItem.query.all().first()
+        self.assertEqual(db_item.name, "Skydive")
+        self.assertEqual(data['name'], "Sky dive")
+        self.assertNotEqual(data['data_modified'], data['date_created'])
