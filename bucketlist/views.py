f8061a150934229e8d04c173222b9857802e1ed2
DomieBett <dbett49@gmail.com>
2017-07-24 11:12:27 +0300

    [Fix] Reduce repetition by moving frequently used code to tools module

--- bucketlist/views.py
+++ bucketlist/views.py
@@ -9,13 +9,14 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     from .models import User, BucketList, ListItem
+    from .tools.tools import get_user, bucketlist_data,\
+        doesnt_exist
 
     app = Flask(__name__)
     api = Api(app)
     app.config.from_object(config[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
 
     @api.route('/auth/register')
     class Register(Resource):
@@ -26,34 +27,37 @@ def create_app(config_name):
 
             post_data = request.get_json()
             if not post_data.get('email'):
-                return jsonify({'status': 'fail',
-                                'message': 'Please enter an email to proceed'})
-            user = User.query.filter_by(email=post_data.get('email')).first()
-            if not user:
-                try:
-                    name = post_data.get('username')
-                    email = post_data.get('email')
-                    password = post_data.get('password')
-                    user = User(name, email, password)
-                    user.save()
+                return jsonify({
+                    'status': 'fail',
+                    'message': 'Please enter an email to proceed'
+                })
 
-                    auth_token = user.encode_auth_token(user.id)
-                    response = {
-                        'status': 'success',
-                        'message': 'Successfully registered.',
-                        'auth_token': auth_token.decode()
-                    }
-                    return jsonify(response)
-                except Exception as e:
-                    response = {
-                        'status': 'fail',
-                        'message': 'Some error occurred. Please try again.'
-                    }
-                    return jsonify(response)
-            else:
+            user = User.query.filter_by(email=post_data.get('email')).first()
+            if user:
                 response = {
                     'status': 'fail',
                     'message': 'User already exists. Please Log in.',
+                }
+                return jsonify(response)
+
+            try:
+                name = post_data.get('username')
+                email = post_data.get('email')
+                password = post_data.get('password')
+                user = User(name, email, password)
+                user.save()
+
+                auth_token = user.encode_auth_token(user.id)
+                response = {
+                    'status': 'success',
+                    'message': 'Successfully registered.',
+                    'auth_token': auth_token.decode()
+                }
+                return jsonify(response)
+            except Exception as e:
+                response = {
+                    'status': 'fail',
+                    'message': 'Some error occurred. Please try again.'
                 }
                 return jsonify(response)
 
@@ -94,278 +98,153 @@ def create_app(config_name):
 
     @api.route('/bucketlists/')
     class BucketLists(Resource):
+
+        @api.header('Authorization', 'JWT Token', required=True)
         def get(self):
 
             """Return all bucketlists in the system."""
-            auth_header = request.headers.get('Authorization')
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-            else:
-                auth_token = ''
-            if auth_token:
-                user_id = User.decode_auth_token(auth_token)
-                if not isinstance(user_id, str):
-                    response_obj = []
-                    user = User.query.filter_by(id=user_id).first()
-                    bucketlists = user.bucketlists.all()
+            auth_token = request.headers.get("Authorization")
+            user = get_user(auth_token)
 
-                    for bucketlist in bucketlists:
-                        itemscontent = [{'id': item.id,
-                                         'name': item.name,
-                                         'date_created': item.date_created,
-                                         'date_modified': item.date_modified,
-                                         'done': False, }
-                                        for item in bucketlist.items.all()]
-                        resp = {
-                            'id' : bucketlist.id,
-                            'name' : bucketlist.name,
-                            'items' : itemscontent,
-                            'date_created' : bucketlist.date_created,
-                            'date_modified' : bucketlist.date_modified,
-                            'created_by' : bucketlist.created_by
-                        }
-                        response_obj.append(resp)
-                    return jsonify(response_obj)
-
-                response_obj = {
-                    'status': 'fail',
-                    'message': user_id
-                }
-                return jsonify(response_obj)
-            else:
-                response_obj = {
-                    'status': 'fail',
-                    'message': 'Provide a valid auth token.'
-                }
+            if isinstance(user, User):
+                bucketlists = user.bucketlists.all()
+                if not bucketlists:
+                    return {
+                        'message': 'User has no bucketlists',
+                    }
+                response_obj = []
+                for bucketlist in bucketlists:
+                    resp = bucketlist_data(bucketlist)
+                    response_obj.append(resp)
                 return jsonify(response_obj)
 
+            else:
+                return user, 404
+
+        @api.header('Authorization', 'JWT Token', required=True)
         def post(self):
 
             """Adds a bucketlist."""
 
-            auth_header = request.headers.get("Authorization")
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-            else:
-                auth_token = ""
+            auth_token = request.headers.get("Authorization")
+            user = get_user(auth_token)
 
-            if auth_token:
-                user_id = User.decode_auth_token(auth_token)
-                if not isinstance(user_id, str):
-                    post_data = request.get_json()
-                    user = User.query.filter_by(id=user_id).first()
-                    bucketlist = BucketList(name=post_data.get('name'), owner=user,
-                                            created_by=user.email)
-                    bucketlist.save()
+            if isinstance(user, User):
+                post_data = request.get_json()
+                bucketlist = BucketList(name=post_data.get('name'), owner=user,
+                                        created_by=user.email)
+                bucketlist.save()
 
-                    response_obj = []
-                    bucketlists = user.bucketlists.all()
-                    for bucketlist in bucketlists:
-                        itemscontent = [{'id': item.id,
-                                         'name': item.name,
-                                         'date_created': item.date_created,
-                                         'date_modified': item.date_modified,
-                                         'done': False, }
-                                        for item in bucketlist.items.all()]
-                        resp = {
-                            'id': bucketlist.id,
-                            'name': bucketlist.name,
-                            'items': itemscontent,
-                            'date_created': bucketlist.date_created,
-                            'date_modified': bucketlist.date_modified,
-                            'created_by': bucketlist.created_by
-                        }
-                        response_obj.append(resp)
-                    return jsonify(response_obj)
-                response_obj = {
-                    'status': 'fail',
-                    'message': user_id
-                }
-                return jsonify(response_obj)
-            else:
-                response_obj = {
-                    'status': 'fail',
-                    'message': 'Provide a valid auth token.'
-                }
+                response_obj = []
+                bucketlists = user.bucketlists.all()
+                for bucketlist in bucketlists:
+                    resp = bucketlist_data(bucketlist)
+                    response_obj.append(resp)
                 return jsonify(response_obj)
 
+            else:
+                return user, 404
 
     @api.route('/bucketlists/<id>')
     class SingleBucketList(Resource):
 
+        @api.header('Authorization', 'JWT Token', required=True)
         def get(self, id):
 
             """Returns a single bucket list with the id"""
 
-            auth_header = request.headers.get('Authorization')
+            auth_token = request.headers.get("Authorization")
+            user = get_user(auth_token)
 
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-
-            else:
-                auth_token = ''
-
-            if auth_token:
-                user_id = User.decode_auth_token(auth_token)
-
-                if not isinstance(user_id, str):
-                    user = User.query.filter_by(id=user_id).first()
-                    bucketlist = user.bucketlists.filter_by(id=id).first()
-                    itemscontent = [{'id':item.id,
-                                     'name':item.name,
-                                     'date_created':item.date_created,
-                                     'date_modified':item.date_modified,
-                                     'done': False,}
-                                    for item in bucketlist.items.all()]
-
-                    response_obj = {
-                        'id': bucketlist.id,
-                        'name': bucketlist.name,
-                        'items': itemscontent,
-                        'date_created': bucketlist.date_created,
-                        'date_modified': bucketlist.date_modified,
-                        'created_by': bucketlist.created_by
-                    }
-
-                    return jsonify(response_obj)
-
-                response_obj = {
-                    'status': 'fail',
-                    'message': user_id
-                }
-
+            if isinstance(user, User):
+                bucketlist = user.bucketlists.filter_by(id=id).first()
+                if not bucketlist:
+                    return doesnt_exist("BucketList"), 404
+                response_obj = bucketlist_data(bucketlist)
                 return jsonify(response_obj)
             else:
-                response_obj = {
-                    'status': 'fail',
-                    'message': 'Provide a valid auth token.'
-                }
-                return jsonify(response_obj)
+                return user, 404
 
+        @api.header('Authorization', 'JWT Token', required=True)
         def delete(self, id):
 
             """Deletes bucketlist with the primary key of 'id'."""
-            auth_header = request.headers.get('Authorization')
+            auth_token = request.headers.get("Authorization")
+            user = get_user(auth_token)
 
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-            else:
-                auth_token = ''
-
-            if auth_token:
-                user_id = User.decode_auth_token(auth_token)
-
-                if not isinstance(user_id, str):
-                    user = User.query.filter_by(id=user_id).first()
-                    bucketlist = user.bucketlists.filter_by(id=id).first()
-                    bucketlist.delete()
-
-                    response_obj = {
-                        'status': 'success',
-                        'message': 'Successfully deleted.',
-                        'id': bucketlist.id,
-                    }
-                    return jsonify(response_obj)
+            if isinstance(user, User):
+                bucketlist = user.bucketlists.filter_by(id=id).first()
+                if not bucketlist:
+                    doesnt_exist("BucketList"), 404
+                bucketlist.delete()
 
                 response_obj = {
-                    'status': 'fail',
-                    'message': user_id
+                    'status': 'success',
+                    'message': 'Successfully deleted.',
+                    'id': bucketlist.id,
                 }
                 return jsonify(response_obj)
 
             else:
-                response_obj = {
-                    'status': 'fail',
-                    'message': 'Provide a valid auth token.'
-                }
-                return jsonify(response_obj)
+                return user, 404
 
     @api.route('/bucketlists/<id>/items')
     class BucketListItem(Resource):
 
+        @api.header('Authorization', 'JWT Token', required=True)
         def post(self, id):
 
             """Adds item to bucketlist with the id"""
 
-            auth_header = request.headers.get("Authorization")
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-            else:
-                auth_token = ""
+            auth_token = request.headers.get("Authorization")
+            user = get_user(auth_token)
 
-            if auth_token:
-                user_id = User.decode_auth_token(auth_token)
-
-                if not isinstance(user_id, str):
-                    post_data = request.get_json()
-                    user = User.query.filter_by(id=user_id).first()
-                    bucketlist = user.bucketlists.filter_by(id=id).first()
-                    list_item = ListItem(name=post_data.get('content'), bcktlst=bucketlist)
-                    list_item.save()
-
-                    response_obj = {
-                        'status': 'success',
-                        'name': list_item.name,
-                        'bucketlist_id': bucketlist.id,
-                        'message': 'Item added successfully'
-                    }
-                    return jsonify(response_obj)
+            if isinstance(user, User):
+                post_data = request.get_json()
+                bucketlist = user.bucketlists.filter_by(id=id).first()
+                if not bucketlist:
+                    return doesnt_exist("BucketList"), 404
+                list_item = ListItem(name=post_data.get('content'), bcktlst=bucketlist)
+                list_item.save()
 
                 response_obj = {
-                    'status': 'fail',
-                    'message': user_id
+                    'status': 'success',
+                    'name': list_item.name,
+                    'bucketlist_id': bucketlist.id,
+                    'message': 'Item added successfully'
                 }
                 return jsonify(response_obj)
 
             else:
-                response_obj = {
-                    'status': 'fail',
-                    'message': 'Provide a valid auth token.'
-                }
-                return jsonify(response_obj)
+                return user
 
     @api.route('/bucketlists/<id>/items/<item_id>')
     class BucketListItems(Resource):
 
+        @api.header('Authorization', 'JWT Token', required=True)
         def delete(self, id, item_id):
 
             """Deletes items from bucketlists with id as item id."""
 
-            auth_header = request.headers.get('Authorization')
+            auth_token = request.headers.get("Authorization")
+            user = get_user(auth_token)
 
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-            else:
-                auth_token = ''
-
-            if auth_token:
-                user_id = User.decode_auth_token(auth_token)
-
-                if not isinstance(user_id, str):
-                    user = User.query.filter_by(id=user_id).first()
-                    bucketlist = user.bucketlists.filter_by(id=id).first()
-                    item = bucketlist.items.filter_by(id=item_id).first()
-                    item.delete()
-
-                    response_obj = {
-                        'status': 'success',
-                        'message': 'Successfully deleted.',
-                        'bucketlist_id': bucketlist.id,
-                        'id': item.id,
-                    }
-                    return jsonify(response_obj)
+            if isinstance(user, User):
+                bucketlist = user.bucketlists.filter_by(id=id).first()
+                if not bucketlist:
+                    return doesnt_exist("BucketList"), 404
+                item = bucketlist.items.filter_by(id=item_id).first()
+                if not item:
+                    return doesnt_exist("Item"), 404
+                item.delete()
 
                 response_obj = {
-                    'status': 'fail',
-                    'message': user_id
-                }
+                    'status': 'success',
+                    'message': 'Successfully deleted.',
+                }, 200
                 return jsonify(response_obj)
 
             else:
-                response_obj = {
-                    'status': 'fail',
-                    'message': 'Provide a valid auth token.'
-                }
-                return jsonify(response_obj)
+                return user
 
     return app
