eda625a9e70c2607818b63b088d180acbfd4f5ee
DomieBett <dbett49@gmail.com>
2017-07-25 21:12:36 +0300

    [Fix] Edit import statement to be inline with code changes

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from bucketlist.views import create_app
+from bucketlist import create_app
 from bucketlist.models import db
 
 app = create_app('dev')
