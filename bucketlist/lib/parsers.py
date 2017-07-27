c666db37b4478b5954d98327a1f1c24d580f7a6f
DomieBett <dbett49@gmail.com>
2017-07-27 17:56:33 +0300

    [Fix] Change default returned bucketlists to 20

--- bucketlist/lib/parsers.py
+++ bucketlist/lib/parsers.py
@@ -3,5 +3,5 @@ from flask_restplus import reqparse
 paginate_or_search = reqparse.RequestParser()
 paginate_or_search.add_argument('page', type=int, required=False, default=1, help='Page number')
 paginate_or_search.add_argument('limit', type=int, required=False,
-                   default=10, help='Results per page {error_msg}')
+                   default=20, help='Results per page {error_msg}')
 paginate_or_search.add_argument('q')
