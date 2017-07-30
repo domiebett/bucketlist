ffdcd297697cdfb5bb41dba90a30085096ff59c9
DomieBett <dbett49@gmail.com>
2017-07-30 16:35:52 +0300

    [ Chore] Properly format docstrings and comments in code

--- bucketlist/bucketlist/views.py
+++ bucketlist/bucketlist/views.py
@@ -14,24 +14,29 @@ ns = api.namespace('bucketlists', description='Bucketlist related operations')
 @api.doc(params={'limit': 'search limit'})
 class BucketLists(Resource):
 
+    """Retrieves and adds bucketlists."""
+
     @api.header('Authorization', 'JWT Token', required=True)
     @api.response(404, 'bucketlist not found')
     @api.response(401, 'Unauthorised access')
     @api.marshal_list_with(bucket_list)
     def get(self):
 
-        """Receives get request and returns all bucketlists
-        that belong to logged in user"""
+        """Receives get request and returns all bucketlists"""
 
+        # Gets authtoken to retrieve user if token is valid.
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
         if not isinstance(user, User):
             abort(401, user)
 
+        # Retrieves paginate, page or search arguments.
         args = paginate_or_search.parse_args(request)
         page = args.get('page', 1)
         limit = args.get('limit')
         q = args.get('q')
+
+        # Returns paginated or paginated search results
         if q:
             bucketlists = user.bucketlists.filter(
                 BucketList.name.ilike('%'+q+'%'))\
@@ -39,9 +44,11 @@ class BucketLists(Resource):
         else:
             bucketlists = user.bucketlists.paginate(page, limit, False)
 
+        # 404 error if bucketlists dont exist for user
         if not bucketlists:
             abort(404)
 
+        # returns a response with all bucketlists.
         response = []
         for bucketlist in bucketlists.items:
             items = [item for item in bucketlist.items.all()]
@@ -62,19 +69,21 @@ class BucketLists(Resource):
     @api.expect(bucket_list_input)
     def post(self):
 
-        """Receives post request with bucketlist name and
-        adds a bucketlist with given name. Returns the added
-        bucketlist"""
+        """Receives post request and adds a bucketlist."""
 
+        # Gets authtoken to retrieve user if token is valid.
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
 
         if isinstance(user, User):
 
+            # Fetches bucketlist name from request, and creates it.
             post_data = request.get_json()
             bucketlist = BucketList(name=post_data.get('name'), owner=user,
                                     created_by=user.email)
             bucketlist.save()
+
+            #return single bucketlist made.
             items = [item for item in bucketlist.items.all()]
             response = {
                 'id': bucketlist.id,
@@ -93,19 +102,23 @@ class BucketLists(Resource):
 @ns.route('/<int:id>')
 class SingleBucketList(Resource):
 
+    """Operations dealing with manipulating single bucketlist."""
+
     @api.header('Authorization', 'JWT Token', required=True)
     @api.response(404, 'bucketlist doesnt exist')
     @api.response(401, 'Unauthorised access')
     @api.marshal_with(bucket_list)
     def get(self, id):
 
-        """Receives get request with a bucketlist id and returns the
-        bucketlist"""
+        """Receives get request and returns the single bucketlist"""
 
+        # Get token and decode it to return user
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
 
         if isinstance(user, User):
+
+            # Return response with single bucketlist.
             bucketlist = user.bucketlists.filter_by(id=id).first()
             if not bucketlist:
                 abort(404)
@@ -121,18 +134,22 @@ class SingleBucketList(Resource):
     @api.expect(bucket_list_input)
     def put(self, id):
 
-        """Receives a put request with bucketlist id, updates the
-        bucketlist and returns the bucketlist."""
+        """Receives a put request, updates the bucketlist"""
 
+        # Decodes token to retrieve user.
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
 
         if isinstance(user, User):
+
+            # Retrieve bucketlist with the id.
             put_data = request.get_json()
             bucketlist = user.bucketlists.filter_by(id=id).first()
 
             if not bucketlist:
                 abort(404)
+
+            # Modify bucketlist name.
             bucketlist.modify_name(put_data['name'])
             bucketlist = user.bucketlists.filter_by(id=id).first()
             return bucket_content(bucketlist), 201
@@ -145,17 +162,21 @@ class SingleBucketList(Resource):
     @api.response(404, 'Bucketlist doesnt exist')
     def delete(self, id):
 
-        """Receives a delete request with a bucketlist id and
-        deletes the bucketlist. Returns message is succesful"""
+        """Receives a delete request and deletes a bucketlist."""
+
+        # Decode auth_token to retrieve user.
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
 
         if isinstance(user, User):
+
+            # Retrieve bucketlist and delete it.
             bucketlist = user.bucketlists.filter_by(id=id).first()
             if not bucketlist:
                 abort(404)
             bucketlist.delete()
 
+            # Return success message.
             response = {
                 'status': 'success',
                 'message': 'Successfully deleted.',
@@ -169,26 +190,32 @@ class SingleBucketList(Resource):
 @ns.route('/<int:id>/items')
 class BucketListItem(Resource):
 
+    """Deal with addition of bucketlist item"""
+
     @api.header('Authorization', 'JWT Token', required=True)
     @api.marshal_with(bucket_list_items)
     @api.response(404, 'Bucketlist doesnt exist')
     @api.expect(bucket_item_input)
     def post(self, id):
 
-        """Receives post request with bucketlist id and item name.
-        Adds a bucketlist item and returns it."""
+        """Receives post request adds a bucketlist item."""
 
+        # Decode auth_token to get user.
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
 
         if isinstance(user, User):
+
+            # Retrieve item name from request and create item.
             post_data = request.get_json()
             bucketlist = user.bucketlists.filter_by(id=id).first()
             if not bucketlist:
                 abort(404)
-            list_item = ListItem(name=post_data.get('name'), bcktlst=bucketlist)
+            list_item = ListItem(name=post_data.get('name'),
+                                 bcktlst=bucketlist)
             list_item.save()
 
+            # Return the item created.
             return list_item, 201
 
         else:
@@ -197,6 +224,7 @@ class BucketListItem(Resource):
 @ns.route('/<int:id>/items/<item_id>')
 class BucketListItems(Resource):
 
+    """Operations for manipulating single bucketlist items."""
     @api.header('Authorization', 'JWT Token', required=True)
     @api.marshal_with(bucket_list_items)
     @api.response(401, 'Unauthorised access')
@@ -204,8 +232,7 @@ class BucketListItems(Resource):
     @api.expect(bucket_item_input)
     def put(self, id, item_id):
 
-        """Receives put request with id, item id and item name.
-        Modifies the item and returns it."""
+        """Receives put request and modifies the item."""
 
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
@@ -215,10 +242,13 @@ class BucketListItems(Resource):
             bucketlist = user.bucketlists.filter_by(id=id).first()
             if not bucketlist:
                 abort(404, "Bucketlist doesnt exist")
+
+            # Get bucketlist item to modify.
             item = bucketlist.items.filter_by(id=item_id).first()
             if not item:
                 abort(404, "Item doesnt exist")
 
+            # Modify bucketlist item.
             if put_data['name']:
                 item.modify_name(put_data['name'])
             if put_data['done']:
@@ -233,13 +263,14 @@ class BucketListItems(Resource):
     @api.response(404, 'Bucketlist or Item doesnt exist')
     def delete(self, id, item_id):
 
-        """Recieves delete request with bucketlist id and bucket item
-        id. Deletes the item."""
+        """Recieves delete request and deletes a bucketlist item."""
 
         auth_token = request.headers.get("Authorization")
         user = get_user(auth_token)
 
         if isinstance(user, User):
+
+            # Retrieve bucketlist item and deletes it.
             bucketlist = user.bucketlists.filter_by(id=id).first()
             if not bucketlist:
                 abort(404, "Bucketlist doesnt exist")
@@ -248,6 +279,7 @@ class BucketListItems(Resource):
                 abort(404, "Item doesnt exist")
             item.delete()
 
+            # Return success message.
             response_obj = {
                 'status': 'success',
                 'message': 'Successfully deleted.',
