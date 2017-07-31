1f4ee5ad25f0e1a7c2c1d99425f2d6eacd99e0a3
DomieBett <dbett49@gmail.com>
2017-07-31 22:22:23 +0300

    [Chore] Add option to get config name from environment config

--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
 #env/bin/.Python
 
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from bucketlist import create_app
 from bucketlist.models import db
 
-app = create_app('dev')
+
+config_name = os.environ.get("APP_CONFIG")
+
+app = create_app(config_name)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
