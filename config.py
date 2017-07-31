1367ed240cb6659a9e6c21d8ef03204de26de718
DomieBett <dbett49@gmail.com>
2017-07-31 22:23:32 +0300

    [Chore] Add retrieval of config data from environment variables.

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+APP_CONFIG = os.environ.get("APP_CONFIG")
+
 class Config:
     APP_NAME = "BucketList"
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hiddenkey"
 
 class TestConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/test_bucketlist"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL")
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://flask:@localhost/bucketlist_api"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL")
 
 class ProdConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "postgressql://dominicbett:@localhost/bucketlist_api"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 config = {
