d9c70c18335aab946b8609a127f29aeb5197fd7d
DomieBett <dbett49@gmail.com>
2017-08-29 19:11:14 +0300

    [Fix] Add CORS header to fix cors error

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
 from config import config
 from bucketlist.restplus import api
 from bucketlist.models import db, User, BucketList, ListItem
@@ -8,6 +10,8 @@ from bucketlist.bucketlist.views import ns as bucketlist_ns
 def create_app(config_name):
 
     app = Flask(__name__)
+    CORS(app)
+    app.url_map.strict_slashes = False
     api.add_namespace(auth_ns)
     api.add_namespace(bucketlist_ns)
     api.init_app(app)
